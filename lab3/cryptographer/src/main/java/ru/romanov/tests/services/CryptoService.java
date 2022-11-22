df6004a5744178342a3812c834c22b34dc3e2872
Романов Илья Олегович <ioromanov@sberinsur.ru>
2022-11-22 09:31:59 +0300

    Шифрую файлы, дешифровать не получается

--- lab3/cryptographer/src/main/java/ru/romanov/tests/services/CryptoService.java
+++ lab3/cryptographer/src/main/java/ru/romanov/tests/services/CryptoService.java
@@ -1,21 +1,123 @@
 package ru.romanov.tests.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import static ru.romanov.tests.utils.JsonUtils.getJson;
 
 
 @Service
+@Slf4j
 public class CryptoService {
 
     private String pathToFile = "/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files/src/users.json";
 
-    public boolean encryptFile(String key) {
+    public boolean encryptFile(String userKeys) {
+        log.info("Пришёл ключ: " + userKeys);
+        String json = getJson(pathToFile);
+        byte[] arrJson = json.getBytes(StandardCharsets.UTF_8);
+        byte[] arrUserKey = userKeys.getBytes(StandardCharsets.UTF_8);
+        byte[] cipherJson = null;
 
-        return false;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+
+            String algorithm = "AES";
+            SecretKeySpec key = new SecretKeySpec(arrUserKey, algorithm);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            log.info("Инициализируем шифровальщик");
+
+            cipherJson = cipher.doFinal(arrJson);
+            log.info("Зашифровали файл");
+
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+
+        /*"/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files"*/
+
+        String FILE_PATH = "/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files/src/cipherFile.txt";
+
+        try (FileOutputStream outputStream = new FileOutputStream(FILE_PATH)) {
+            FileChannel.open(Paths.get(FILE_PATH), StandardOpenOption.WRITE).truncate(0).close();
+            outputStream.write(cipherJson);
+            log.info("Записали шифрованные данные во временный файл cipherFile.txt");
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public boolean decryptFile(String key) {
+    public boolean decryptFile(String userKeys) {
+        log.info("Пришёл ключ: " + userKeys);
+        /**
+         *
+         */
+        byte[] arrUserKey = userKeys.getBytes(StandardCharsets.UTF_8);
+        byte[] cipherJson = null;
+        byte[] array = new byte[0];
+        try {
+            array = Files.readAllBytes(Paths.get("/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files/src/cipherFile.txt"));
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
-        return false;
+
+            String algorithm = "AES";
+            SecretKeySpec key = new SecretKeySpec(arrUserKey, algorithm);
+
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            log.info("Инициализируем дешифровальщик");
+
+            cipherJson = cipher.doFinal(array);
+            log.info("Дешифровали файл");
+        } catch (IOException | NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException |
+                 BadPaddingException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+        /*"/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files"*/
+
+        String FILE_PATH = "/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files/temp/users.json";
+
+        try (FileOutputStream outputStream = new FileOutputStream(FILE_PATH)) {
+            FileChannel.open(Paths.get(FILE_PATH), StandardOpenOption.WRITE).truncate(0).close();
+            outputStream.write(cipherJson);
+            log.info("Записали дешифрованные данные во временный файл users.json");
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void writePasswordToFile(String key) {
+        String FILE_PATH = "/home/ioromanov/ideaProjects/cyber-sec/cyber-sec/lab3/files/password/pswrd.txt";
+        try (PrintWriter out = new PrintWriter(new FileWriter(FILE_PATH))) {
+            FileChannel.open(Paths.get(FILE_PATH), StandardOpenOption.WRITE).truncate(0).close();
+            out.write(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- lab3/cryptographer/src/main/java/ru/romanov/tests/views/MainView.java
+++ lab3/cryptographer/src/main/java/ru/romanov/tests/views/MainView.java
@@ -9,13 +9,28 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.romanov.tests.services.CryptoService;
 
+import java.util.UUID;
+
 @PageTitle("Прохождение тестов")
 @Route(value = "/", layout = MainLayout.class)
 public class MainView extends VerticalLayout {
 
+    /**
+     * Кнопка для шифрования
+     */
     private Button encryptButton = new Button();
+    /**
+     * Кнопка для дешифрования
+     */
     private Button decryptButton = new Button();
+    /**
+     * Кнопка для ввода парольной фразы
+     */
     private TextField textField = new TextField();
+    /**
+     * Кнопка для генерации парольной фразы
+     */
+    private Button generateKeyButton = new Button();
 
     private CryptoService cryptoService;
 
@@ -28,28 +43,40 @@ public class MainView extends VerticalLayout {
         Div div = new Div();
         div.setWidth("700px");
 
+        generateKeyButton.setText("Сгенерировать пароль");
+        generateKeyButton.addClickListener(event -> {
+            String s = UUID.randomUUID().toString().substring(0, 32);
+            textField.setValue(s);
+        });
+        generateKeyButton.getStyle().set("margin-right", "130px");
+
         encryptButton.setText("Шифровать");
         encryptButton.addClickListener(event -> {
-            cryptoService.encryptFile("sfgjs");
+            String value = textField.getValue();
+            cryptoService.encryptFile(value);
             textField.clear();
         });
         encryptButton.getStyle().set("margin-right", "15px");
 
 
         decryptButton.setText("Дешифровать");
-        decryptButton.addClickListener(event ->{
+        decryptButton.addClickListener(event -> {
+            cryptoService.decryptFile(textField.getValue());
             textField.clear();
         });
         decryptButton.getStyle().set("margin-right", "15px");
 
         textField.setLabel("Введите ключ для шифрования/дешифрования");
+        textField.setPlaceholder("Введите пароль");
         textField.setWidth("320px");
         textField.getStyle().set("margin-right", "15px");
 
         div.add(encryptButton, textField, decryptButton);
+        Div div1 = new Div();
+        div1.add(generateKeyButton);
 
         VerticalLayout layout = new VerticalLayout();
-        layout.add(div);
+        layout.add(div, div1);
         layout.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
         return layout;
